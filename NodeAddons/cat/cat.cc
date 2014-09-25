9a61b864b0dc5fb7071534f99865a8a234204e1d
Ravi Suhag <suhag.ravi@gmail.com>
2014-09-25 16:13:19 +0530

    restructing files

--- NodeAddons/cat/cat.cc
+++ NodeAddons/cat/cat.cc
@@ -8,9 +8,18 @@ Handle<Value> Method(const Arguments& args) {
   return scope.Close(String::New("Meowwwwwww.... !"));
 }
 
+
+// Here, we take the target, which is presumably the Node.js binding, and set a property on it.
+// We are using V8 to templatize and instantiate a new function that we’ve called “Method”
+
 void init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("speak"),
       FunctionTemplate::New(Method)->GetFunction());
 }
+
+
+
+// Here we declare the NODE_MODULE.  
+// It’s called “nodecat”, and it’s entry point is “init”.
 
 NODE_MODULE(nodecat, init)
