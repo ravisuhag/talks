72cc074a652bbe6a05227325f86e6d0cd4f52191
Ravi Suhag <suhag.ravi@gmail.com>
2014-09-25 14:53:27 +0530

    Adding cat speak module in node

---
+++ NodeAddons/cat.js
@@ -0,0 +1,3 @@
+module.exports.speak = function() {
+  return ' Meowwwwwwwww.....!';
+};
---
+++ NodeAddons/server.js
@@ -0,0 +1,4 @@
+var cat= require("./cat.js");
+
+console.log("\033[32m Cat is about to speak now ..... ! \033[0m");
+console.log(cat.speak());
